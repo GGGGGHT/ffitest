3f7ef0f2d5b44b1ebf5437800b46ca18a2427925
wangzheng <wz9712203617@gmail.com>
2024-11-27 11:04:32 +0800

    misc: 修改README文档

--- Crash.java
+++ Crash.java
@@ -2,20 +2,27 @@ import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.net.URLStreamHandler;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.foreign.ValueLayout.*;
 
 
 public class Crash {
+	public static Arena GLOBAL = Arena.global();
 	private static final int JVMTI_ERROR_NONE = 0;
 	static SymbolLookup symbolLookup = SymbolLookup.loaderLookup();
 	static Linker linker = Linker.nativeLinker();
 	static FunctionDescriptor JNI_GetCreatedJavaVMs_DESCRIPTOR = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_INT, ValueLayout.ADDRESS);
 	static FunctionDescriptor GET_JNIENV_DESCRIPTOR = FunctionDescriptor.of(ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT);
 	static FunctionDescriptor GetEnv_DESCRIPTOR = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT);
+	
 	
 	static {
 		System.setProperty("java.library.path", ".");
@@ -37,103 +44,11 @@ public class Crash {
 	public static native int getVersion();
 	
 	public static void test(MemorySegment classAddress) throws NoSuchMethodException, IllegalAccessException {
-		var arena = Arena.global();
-		// 获取 JavaVM 指针
-		MemorySegment javaVMPtr = getJavaVM();
-		if (javaVMPtr == null) {
-			System.out.println("Failed to get JavaVM pointer");
-			return;
-		}
-		
-		var functions = JavaVM_.functions(javaVMPtr);
-		
-		// 获取 JVMTI 环境
-		MemorySegment jvmtiEnv = getJVMTIEnv(javaVMPtr, functions);
-		if (jvmtiEnv == null) {
-			System.out.println("Failed to get JVMTI environment");
-			return;
-		}
-		MemorySegment jvmtiFunctions = _jvmtiEnv.functions(jvmtiEnv);
-		
-		// 分配内存用于存储版本号
-		MemorySegment versionPtr = arena.allocate(JAVA_INT);
-		
-		MemorySegment getVersionNumber = jvmtiInterface_1_.GetVersionNumber(jvmtiFunctions);
-		var result = jvmtiInterface_1_.GetVersionNumber.invoke(getVersionNumber, jvmtiEnv, versionPtr);
-		// 调用 GetVersionNumber
-		// int result = GetVersionNumber(jvmtiEnv, versionPtr);
-		
-		if (result == 0) {
-			int version = versionPtr.get(JAVA_INT, 0);
-			System.out.printf("JVMTI Version: %d\n", version);
-		} else {
-			System.out.printf("outor getting JVMTI version: %d\n", result);
-		}
-		
-		
-		MemorySegment addCapabilities = jvmtiInterface_1_.AddCapabilities(jvmtiFunctions);
-		MemorySegment cap = arena.allocate(jvmtiCapabilities.sizeof());
-		cap.set(JAVA_SHORT, 0L, (short) 1);
-		
-		int addCapabilitiesResult = jvmtiInterface_1_.AddCapabilities.invoke(addCapabilities, jvmtiEnv, cap);
-		System.out.println("add cap result: " + addCapabilitiesResult);
-		
-		
-		// typedef jvmtiIterationControl (JNICALL *jvmtiHeapObjectCallback)
-		//     (jlong class_tag,
-		//      jlong size,
-		//      jlong* tag_ptr,
-		//      void* user_data);
-		// MemorySegment mh = jvmtiInterface_1_.IterateOverInstancesOfClass.allocate(f, arena);
-		MemorySegment mem = jvmtiHeapObjectCallback.allocate((_, _, _, _) -> 0, arena);
-		
-		MemorySegment interClass = jvmtiInterface_1_.IterateOverInstancesOfClass(jvmtiFunctions);
-		// MemorySegment i = arena.allocate(JAVA_INT);
-		// i.set(JAVA_INT, 0, 0);
-		int invoke = jvmtiInterface_1_.IterateOverInstancesOfClass.invoke(interClass,
-				jvmtiEnv,
-				classAddress.reinterpret(8),
-				3,
-				arena.allocate(JAVA_INT),
-				MemorySegment.NULL
-		);
-		
-		System.out.println("iterator result: " + invoke);
-		
+	
 	}
 	
-	private static MemorySegment getJavaVM() {
-		Arena arena = Arena.global();
-		// 分配内存用于存储 JavaVM 指针
-		MemorySegment vmPtr = arena.allocate(ADDRESS);
-		MemorySegment numVMs = arena.allocate(ValueLayout.JAVA_INT);
-		numVMs.set(ValueLayout.JAVA_INT, 0, 0);
-		
-		// 调用 JNI_GetCreatedJavaVMs
-		int result = jvmti_h.JNI_GetCreatedJavaVMs(vmPtr, 1, numVMs);
-		
-		if (result == 0) {
-			return vmPtr.get(ADDRESS, 0).reinterpret(JavaVM_.sizeof());
-		}
-		return null;
-	}
-	
-	private static MemorySegment getJVMTIEnv(MemorySegment vm, MemorySegment functions) {
-		Arena arena = Arena.global();
-		// 分配内存用于存储 JVMTI 环境指针
-		MemorySegment envPtr = arena.allocate(ADDRESS);
-		// 调用 GetEnv 获取 JVMTI 环境
-		MemorySegment env = JNIInvokeInterface_.GetEnv(functions);
-		int result = JNIInvokeInterface_.GetEnv.invoke(env, vm, envPtr, 805371904);
-		if (result == 0) {
-			return envPtr.get(ADDRESS, 0).reinterpret(_jvmtiEnv.sizeof());
-		}
-		return null;
-		
-	}
-	
-	
-	void main() throws Throwable {
+
+	public static void main(String[] args) throws Throwable {
 		MemorySegment jniGetCreatedJavaVM_FP = symbolLookup.find("JNI_GetCreatedJavaVMs").get();
 		MethodHandle JNI_GetCreatedJavaVM_MH = linker.downcallHandle(JNI_GetCreatedJavaVMs_DESCRIPTOR).bindTo(jniGetCreatedJavaVM_FP);
 		Arena global = Arena.global();
@@ -166,11 +81,13 @@ public class Crash {
 		
 		MemorySegment findClassFP = JNINativeInterface_.FindClass(JNI_Functions);
 		Class c = ThreadPoolExecutor.class;
+		
+		System.out.println(c.getName().replace(".","/"));
 		MemorySegment clazzName = global.allocateFrom(c.getName().replace(".", "/"));
 		
-		// MemorySegment classNameFp = JNINativeInterface_.FindClass.invoke(findClassFP, jniEnvPointer, clazzName);
+		MemorySegment classNameFp = JNINativeInterface_.FindClass.invoke(findClassFP, jniEnvPointer, clazzName);
 		MemorySegment memorySegment = FindClass(c).reinterpret(8);
-		// System.out.println("classNameFp: " + classNameFp + ", other: " + memorySegment);
+		System.out.println("classNameFp: " + classNameFp + ", other: " + memorySegment);
 		
 		
 		var GetVersionNumberPointer = jvmti_functions.get(ValueLayout.ADDRESS, ADDRESS_SIZE * 87);
@@ -188,13 +105,9 @@ public class Crash {
 		System.out.println("ffi: " + version.get(ValueLayout.JAVA_INT, 0));
 		System.out.println("FFI: " + newversion.get(ValueLayout.JAVA_INT, 0));
 		test(memorySegment);
-		// getJNINativeInterface_();
 	}
 	
-
-	
-	
-	public MemorySegment FindClass(Class<?> c) throws Throwable {
+	public static MemorySegment FindClass(Class<?> c) throws Throwable {
 		return (MemorySegment) FindClassMH.invokeExact(
 				FindClassFp,
 				jniEnvPointer,
@@ -209,8 +122,4 @@ public class Crash {
 	
 	static MemorySegment FindClassFp;
 	static MemorySegment jniEnvPointer = null;
-	
-	
-	private static final long ADDRESS_SIZE = ValueLayout.ADDRESS.byteSize();
-	
 }
--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 Use Java22 FFI[^1] call JVMTI[^2] function
 
-## java version 
-openjdk version "22" 2024-03-19 <br/>
-OpenJDK Runtime Environment GraalVM CE 22+36.1 (build 22+36-jvmci-b02)<br/>
-OpenJDK 64-Bit Server VM GraalVM CE 22+36.1 (build 22+36-jvmci-b02, mixed mode, sharing)
+## env
+- java version: 23.0.1 <br/>
+- os: CentOS Linux release 7.9.2009 (Core)
 
 ### jextract[^3]
 ```
@@ -19,11 +18,18 @@ gcc -shared -o libmyjvmti.so test.cpp -I${JAVA_HOME}/include -I${JAVA_HOME}/incl
 
 ### run 
 ```
-java --enable-native-access=ALL-UNNAMED --enable-preview --source 22 Crash.java
+java --enable-native-access=ALL-UNNAMED IterateInstance.java
 ```
 
+### end
+> During the execution of this function the state of the heap does not change: no objects are allocated, no objects are garbage collected, and the state of objects (including held values) does not change. As a result, threads executing Java programming language code, threads attempting to resume the execution of Java programming language code, and threads attempting to execute JNI functions are typically stalled.
+![img.png](img.png)
 
+在执行此函数期间，堆的状态不会改变：不会分配任何对象，不会对任何对象进行垃圾回收，并且对象的状态（包括保存的值）不会改变。因此，执行 Java 编程语言代码的线程、尝试恢复 Java 编程语言代码执行的线程以及尝试执行 JNI 函数的线程通常会停滞。
+所以 需要 upcall 的函数均无法通过 FFI 去执行
 
 [^1]: https://openjdk.org/jeps/454
 [^2]: https://docs.oracle.com/javase/8/docs/platform/jvmti/jvmti.html
 [^3]: https://jdk.java.net/jextract
+[^4]: https://docs.oracle.com/en/java/javase/23/docs/specs/jvmti.html#IterateThroughHeap:~:text=class.%20This%20includes%20both%20reachable%20and%20unreachable%20objects.-,The%20object_filter%20parameter%20indicates%20the%20objects%20for%20which%20the%20callback%20function,code%2C%20and%20threads%20attempting%20to%20execute%20JNI%20functions%20are%20typically%20stalled.,-Phase
+
